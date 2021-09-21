446b07fbe30702c52946ea55b4bc661b174653dc
Linda Oscar Gcaba <lindagcaba@gmail.com>
2021-09-21 12:41:36 +0200

    update meb values to incorportate kenya mbz drug

--- materialized_views/mda_lite/007_supervisor_drug_distribution.sql
+++ materialized_views/mda_lite/007_supervisor_drug_distribution.sql
@@ -116,13 +116,23 @@ SELECT
         COALESCE(case when events.form_data ->> 'drugs' = 'MEB' then events.form_data ->> 'treated_male_5_to_14' end,'0')::integer +
         COALESCE(case when events.form_data ->> 'drugs' = 'MEB' then events.form_data ->> 'treated_male_5_to_15' end,'0')::integer +
         COALESCE(case when events.form_data ->> 'drugs' = 'MEB' then events.form_data ->> 'treated_male_above_16' end,'0')::integer +
-        COALESCE(case when events.form_data ->> 'drugs' = 'MEB' then events.form_data ->> 'treated_male_above_15' end,'0')::integer
+        COALESCE(case when events.form_data ->> 'drugs' = 'MEB' then events.form_data ->> 'treated_male_above_15' end,'0')::integer +
+        COALESCE(case when events.form_data ->> 'drugs' = 'MBZ' then events.form_data ->> 'treated_male_1_to_4' end,'0')::integer +
+        COALESCE(case when events.form_data ->> 'drugs' = 'MBZ' then events.form_data ->> 'treated_male_5_to_14' end,'0')::integer +
+        COALESCE(case when events.form_data ->> 'drugs' = 'MBZ' then events.form_data ->> 'treated_male_5_to_15' end,'0')::integer +
+        COALESCE(case when events.form_data ->> 'drugs' = 'MBZ' then events.form_data ->> 'treated_male_above_16' end,'0')::integer +
+        COALESCE(case when events.form_data ->> 'drugs' = 'MBZ' then events.form_data ->> 'treated_male_above_15' end,'0')::integer
         ) as meb_total_male,
     sum(COALESCE(case when events.form_data ->> 'drugs' = 'MEB' then events.form_data ->> 'treated_female_1_to_4' end,'0')::integer +
         COALESCE(case when events.form_data ->> 'drugs' = 'MEB' then events.form_data ->> 'treated_female_5_to_14' end,'0')::integer +
         COALESCE(case when events.form_data ->> 'drugs' = 'MEB' then events.form_data ->> 'treated_female_5_to_15' end,'0')::integer +
         COALESCE(case when events.form_data ->> 'drugs' = 'MEB' then events.form_data ->> 'treated_female_above_16' end,'0')::integer +
-        COALESCE(case when events.form_data ->> 'drugs' = 'MEB' then events.form_data ->> 'treated_female_above_15' end,'0')::integer
+        COALESCE(case when events.form_data ->> 'drugs' = 'MEB' then events.form_data ->> 'treated_female_above_15' end,'0')::integer +
+        COALESCE(case when events.form_data ->> 'drugs' = 'MBZ' then events.form_data ->> 'treated_female_1_to_4' end,'0')::integer +
+        COALESCE(case when events.form_data ->> 'drugs' = 'MBZ' then events.form_data ->> 'treated_female_5_to_14' end,'0')::integer +
+        COALESCE(case when events.form_data ->> 'drugs' = 'MBZ' then events.form_data ->> 'treated_female_5_to_15' end,'0')::integer +
+        COALESCE(case when events.form_data ->> 'drugs' = 'MBZ' then events.form_data ->> 'treated_female_above_16' end,'0')::integer +
+        COALESCE(case when events.form_data ->> 'drugs' = 'MBZ' then events.form_data ->> 'treated_female_above_15' end,'0')::integer
         ) as meb_total_female,
     sum(COALESCE(case when events.form_data ->> 'drugs' = 'MEB' then events.form_data ->> 'treated_male_1_to_4' end,'0')::integer +
         COALESCE(case when events.form_data ->> 'drugs' = 'MEB' then events.form_data ->> 'treated_male_5_to_14' end,'0')::integer +
